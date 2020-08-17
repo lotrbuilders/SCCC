43facf003f7aad205c6eff610fba58aa95853d10
lotrbuilders <daan.2008@hotmail.com>
2020-08-17 12:07:26 +0200

    Update sccc.c

--- src/sccc.c
+++ src/sccc.c
@@ -95,7 +95,7 @@ int main(int argc, char **argv)
 	if(input_file==0)
 	{
 		fprintf(stderr,"Expected an input file\n");
-		exit(-1);
+		return -1;
 	}
 	stripped=strip_suffix(input_file);
 	if(output_file==0)
